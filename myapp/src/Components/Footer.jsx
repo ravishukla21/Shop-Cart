1ee3aeb5c4ffe5b31ad8169a37e65ef2a253a693
Asim <mohdasim.srk@gmailcom>
2023-05-06 18:30:12 +0530

    fw24_477_Day4

--- myapp/src/Components/Footer.jsx
+++ myapp/src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Table,Thead,Tbody,Tr,Td,Th,Flex,Box,Image,Text,Grid,Center} from "@chakra-ui/react"
+import {Table,Thead,Tbody,Tr,Td,Th,Flex,Box,Image,Text,Grid} from "@chakra-ui/react"
 import logo from "../images/ShopCart.png"
 import logo1 from "../images/stripe.png"
 import logo2 from "../images/visa.png"
@@ -16,78 +16,78 @@ const Footer = () => {
   return (
     <Box ml={10} mr={10} >
         <Box borderBottom={"2px solid gray"}></Box>
-    <Flex mt={50} gap={100} >
-    <Box className='left'>
-        <Box>
+    <Flex mt={50} gap={100} flexDirection={{xl:'row',lg:'row',base:'column'}} >
+    <Box className='left' fontFamily={"'Inter', sans-serif"}>
+        <Box cursor='pointer'>
      <Image src={logo} alt="Logo" w={"50%"} />
      </Box>
-     <Text mt={10}>Lorem ipsum dolor sit amet consectetur adipisicing  <br /> pariatur atque rem Libero  atque, aliquam deleniti <br /> alias rem  cupiditate in. Minima!</Text>
+     <Text mt={10} >Lorem ipsum dolor sit amet consectetur adipisicing  <br /> pariatur atque rem Libero  atque, aliquam deleniti <br /> alias rem  cupiditate in. Minima!</Text>
 <Box mt={25} mb={15}>
 
-      <Text   as='b'  fontSize='30px'>Accepted Payments</Text>
+      <Text   as='b'  fontSize={{xl:'xl',lg:'xl',base:'md'}} >Accepted Payments</Text>
 </Box>
-      <Grid templateColumns='repeat(4, 1fr)' gap={3} mb={10} w={"60%"} ml={2}  >
+      <Grid templateColumns={{xl:'repeat(4, 1fr)',lg:'repeat(4, 1fr)',md:'repeat(2, 1fr)',sm:'repeat(1, 1fr)',base:'repeat(1, 1fr)'}} gap={3} mb={10} mt={5} w={{xl:"70%",lg:"70%",base:"30%"}} ml={2}
+       cursor={'pointer'} >
          
-        <Box border="1px solid gray" >
-         <Center>
+        <Box border="1px solid gray" borderRadius={'5px'} p={2} >
+         
 
-     <Image src={logo1} alt="logo"  w={"80%"} h={30} />
-         </Center>
+     <Image src={logo1} alt="logo"  w={{md:"100%",lg:"100%",base:"60%"}} mt={1} ml={{xl:0,lg:0,md:'0',base:'4'}}  />
+        
         </Box>
   
 
-        <Box border="1px solid gray" h={8} >
-           <Center>
-     <Image src={logo2} alt="logo"  w={"80%"} h={5} mt={1}/>
-     </Center>
+        <Box border="1px solid gray" borderRadius={'5px'} p={2} >
+          
+     <Image src={logo2} alt="logo"   w={{md:"100%",lg:"100%",base:"50%"}} h={5} mt={1}/>
+     
         </Box>
   
    
       
-        <Box border="1px solid gray">
-        <Center>
-     <Image src={logo3} alt="logo"  w={"80%"} h={6} mt={1}/>
+        <Box border="1px solid gray"  borderRadius={'5px'} p={2}>
+        
+     <Image src={logo3} alt="logo"   w={{md:"100%",lg:"100%",base:"50%"}} h={6} mt={1}/>
 
-     </Center>
+    
         </Box>
    
    
 
-        <Box border="1px solid gray">
-        <Center>
-     <Image src={logo4} alt="logo"  w={"70%"} h={5} mt={2}/>
-     </Center>
+        <Box border="1px solid gray"  borderRadius={'5px'} p={2}>
+        
+     <Image src={logo4} alt="logo"  w={{md:"70%",lg:"70%",base:"60%"}} h={5} mt={2} ml={1}/>
+    
         </Box>
    
 
 
-        <Box border="1px solid gray" h={8}>
-        <Center>
-     <Image src={logo5} alt="logo"  w={"90%"} h={6} mt={1}/>
-     </Center>
+        <Box border="1px solid gray"   borderRadius={'5px'} p={2}>
+      
+     <Image src={logo5} alt="logo"   w={{md:"90%",lg:"90%",base:"60%"}} h={6} mt={1}/>
+     
         </Box>
 
    
 
-        <Box border="1px solid gray">
-        <Center>
-     <Image src={logo6} alt="logo"  w={"80%"} h={5} mt={1}/>
-     </Center>
+        <Box border="1px solid gray"  borderRadius={'5px'} p={2}>
+     <Image src={logo6} alt="logo"   w={{md:"80%",lg:"80%",base:"60%"}} h={5} mt={1}/>
+     
         </Box>
    
    
 
-        <Box border="1px solid gray" >
-        <Center >
-     <Image src={logo7} alt="logo"  w={"80%"} h={5} mt={1}/>
-     </Center>
+        <Box border="1px solid gray"  borderRadius={'5px'} p={2} >
+        
+     <Image src={logo7} alt="logo"  w={{md:"80%",lg:"80%",base:"60%"}} h={5} mt={1}/>
+     
         </Box>
    
    
-        <Box border="1px solid gray">
-         <Center>
-     <Image src={logo8} alt="logo"  w={"80%"} h={5} mt={1}/>
-         </Center>
+        <Box border="1px solid gray"  borderRadius={'5px'} p={2}>
+         
+     <Image src={logo8} alt="logo"  w={{md:"80%",lg:"80%",base:"60%"}} h={5} mt={1}/>
+         
         </Box>
    
 
@@ -97,98 +97,106 @@ const Footer = () => {
           
         </Box>
  
-    <Box className="right">
+    <Box className="right"   >
     
-  <Table size='sm'>
+  <Table size='sm' w={'wrap'} >
   <Thead  >
     <Tr  >
-        <Th  fontSize='md' >Department</Th>
-        <Th fontSize='md'>About Us</Th>
-        <Th fontSize='md'>Services</Th>
-        <Th fontSize='md'>Help</Th>
+         <Th fontSize={['xs', 'md','sm']}  >Department</Th>
+          <Th fontSize={['xs', 'md','sm']} >About Us</Th>
+         <Th fontSize={['xs', 'md','sm']}  >Services</Th>
+         <Th fontSize={['xs', 'md','sm']} >Help</Th>
     </Tr>
   </Thead>
-  <Tbody>
-    <Tr>
-        <Td>Fashion</Td>
-        <Td>About ShopCart</Td>
-        <Td>Gift Card</Td>
-        <Td>Shopcart Help</Td>
+  <Tbody >
+    <Tr >
+        <Td fontSize={['xs', 'md']}>Fashion</Td>
+        <Td fontSize={['xs', 'md']}>About ShopCart</Td>
+        <Td fontSize={['xs', 'md']}>Gift Card</Td>
+        <Td fontSize={['xs', 'md']}>Shopcart Help</Td>
     </Tr>
     <Tr>
-        <Td>Education Product</Td>
-        <Td>Carrers</Td>
-        <Td>Mobile App</Td>
-        <Td>Returns</Td>
+        <Td fontSize={['xs', 'md']}>Education Product</Td>
+        <Td fontSize={['xs', 'md']}>Carrers</Td>
+        <Td fontSize={['xs', 'md']}>Mobile App</Td>
+        <Td fontSize={['xs', 'md']}>Returns</Td>
 
     </Tr>
     <Tr>
-   <Td>Frozen Food</Td>
-   <Td>News and Blogs</Td>
-   <Td>Shipping and Delivery</Td>
-   <Td>Track Order</Td>
+   <Td fontSize={['xs', 'md']}>Frozen Food</Td>
+   <Td fontSize={['xs', 'md']}>News and Blogs</Td>
+   <Td fontSize={['xs', 'md']}>Shipping and Delivery</Td>
+   <Td fontSize={['xs', 'md']}>Track Order</Td>
     </Tr>
     <Tr>
-    <Td>Beverages</Td>
-    <Td>Help</Td>
-    <Td>Order Pickup</Td>
-    <Td>Contact Us</Td>
+    <Td fontSize={['xs', 'md']}>Beverages</Td>
+    <Td fontSize={['xs', 'md']}>Help</Td>
+    <Td fontSize={['xs', 'md']}>Order Pickup</Td>
+    <Td fontSize={['xs', 'md']}>Contact Us</Td>
     </Tr>
     <Tr>
-      <Td>Organic Grocery</Td>
-      <Td>Press Center</Td>
-      <Td>Account Signup</Td>
-      <Td>Feedback</Td>
+      <Td fontSize={['xs', 'md']}>Organic Grocery</Td>
+      <Td fontSize={['xs', 'md']}>Press Center</Td>
+      <Td fontSize={['xs', 'md']}>Account Signup</Td>
+      <Td fontSize={['xs', 'md']}>Feedback</Td>
     </Tr>
     <Tr>
-      <Td>Office Supplies</Td>
-      <Td>Shop By Location</Td>
-      <Td></Td>
-      <Td>Security & Fraud</Td>
+      <Td fontSize={['xs', 'md']}>Office Supplies</Td>
+      <Td fontSize={['xs', 'md']}>Shop By Location</Td>
+      <Td fontSize={['xs', 'md']}></Td>
+      <Td fontSize={['xs', 'md']}>Security & Fraud</Td>
     </Tr>
     <Tr>
-      <Td>Beauty Products</Td>
-      <Td>Shopcart Brands</Td>
+      <Td fontSize={['xs', 'md']}>Beauty Products</Td>
+      <Td fontSize={['xs', 'md']}>Shopcart Brands</Td>
     </Tr>
     <Tr>
-      <Td>Books</Td>
-      <Td>Affiliate & Partners</Td>
+      <Td fontSize={['xs', 'md']}>Books</Td>
+      <Td fontSize={['xs', 'md']}>Affiliate & Partners</Td>
     </Tr>
     <Tr>
-      <Td>Electronic & Gadget</Td>
-      <Td>Ideas & Guides</Td>
+      <Td fontSize={['xs', 'md']}>Electronic & Gadget</Td>
+      <Td fontSize={['xs', 'md']}>Ideas & Guides</Td>
     </Tr>
     <Tr>
-      <Td>Travel Accesories</Td>
+      <Td fontSize={['xs', 'md']}>Travel Accesories</Td>
     </Tr>
     <Tr>
-      <Td>Fitness</Td>
+      <Td fontSize={['xs', 'md']}>Fitness</Td>
     </Tr>
-    <Tr><Td>Sneakers</Td></Tr>
-    <Tr><Td>Toys</Td></Tr>
-    <Tr><Td>Furniture</Td></Tr>
+    <Tr><Td fontSize={['xs', 'md']}>Sneakers</Td></Tr>
+    <Tr><Td fontSize={['xs', 'md']}>Toys</Td></Tr>
+    <Tr><Td fontSize={['xs', 'md']}>Furniture</Td></Tr>
   </Tbody>
   </Table>
       
     </Box>
 
- 
-
-
     </Flex>
-    <Box borderBottom={"2px solid gray"} mt={10}></Box>
-    <Flex justify={'space-between'} align={"center"} mt={19} mb={20}>
-     <Flex justify={'space-between'} align={"center"} gap={10}>
-      <Flex justify={'space-between'} align={'center'} gap={2}><BsFillBagCheckFill/> Become Seller</Flex>
-      <Flex justify={'space-between'} align={'center'} gap={2}><AiFillGift/> Gift Card</Flex>
-      <Flex justify={'space-between'} align={'center'} gap={2}><FiHelpCircle/> Help Center</Flex>
+    <Box borderBottom={"2px solid gray"} mt={10} ></Box>
+    
+    <Flex justify={'space-between'} align={{base:"center",md:'left',sm:'left'}} mt={19} mb={20} fontFamily={"'Inter', sans-serif"}
+    flexDirection={{base:'column',xl:'row',lg:'row'}}
+    w={'wrap'}
+    
+    > 
+
+     <Flex justify={'space-between'} align={"center"} gap={10}
+     flexDirection={{base:'column',xl:'row',lg:'row',md:'row'}}
+     >
+
+      <Flex justify={'space-between'} align={'center'} gap={2}><BsFillBagCheckFill color='#d14d8f'/> Become Seller</Flex>
+      <Flex justify={'space-between'} align={'center'} gap={2}><AiFillGift  color='#d14d8f'/> Gift Card</Flex>
+      <Flex justify={'space-between'} align={'center'} gap={2}><FiHelpCircle  color='#d14d8f'/> Help Center</Flex>
      </Flex>
-     <Flex justify={'space-between'} align={"center"} gap={10}>
+
+
+     <Grid templateColumns={{xl:'repeat(2,1fr)',lg:'repeat(2,1fr)',base:"repeat(1,1fr)"}} align={"center"} gap={10} >
       <Text>Terms and Services</Text>
       <Text>Privacy and Policy</Text>
-     </Flex>
+     </Grid>
      <Box>
-      <Text>All Right reserved by Museming | 2022 </Text>
+      <Text w={'wrap'}>All Right reserved by Museming | 2022 </Text>
      </Box>
     </Flex>
 
